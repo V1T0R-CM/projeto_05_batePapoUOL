796c838b70594a832b80b6c6e33e9b90d71731e3
Vitor Carvalho Matos <vitorcarvalhomatos@gmail.com>
2022-04-16 17:59:25 -0300

    Tela de entrada completa

--- script.js
+++ script.js
@@ -25,8 +25,6 @@ function renovaStatus(){
 }
 
 function iniciaChat(){
-    document.querySelector(".janela-inicial").classList.add("escondido")
-    document.querySelector(".janela-principal").classList.remove("escondido")
     setInterval(renovaStatus,5000);
     setInterval(carregaChat,3000);
 }
@@ -36,8 +34,21 @@ function carregaChat(){
 }
 
 function carregaMsgs(response){
+    document.querySelector(".janela-inicial").classList.add("escondido")
+    document.querySelector(".janela-principal").classList.remove("escondido")
     msgs= response.data;
     renderizarMsgs();
+}
+
+function converteHora(hora){
+    let horaAtual=Number(hora.slice(0,2))+9
+    if (horaAtual>=24){
+        horaAtual-=24
+    }
+    if(String(horaAtual).length<2){
+        horaAtual="0"+horaAtual
+    }
+    return horaAtual+hora.slice(2,hora.length)
 }
 
 function renderizarMsgs(){
@@ -48,19 +59,19 @@ function renderizarMsgs(){
         if (msgs[i].type==="status"){
             divChat.innerHTML+=`
             <div class="mensagem alerta">
-                (${msgs[i].time}) <strong>${msgs[i].from}</strong> ${msgs[i].text}
+                <span>(${converteHora(msgs[i].time)})</span> <strong>${msgs[i].from}</strong> ${msgs[i].text}
             </div>`;
         }
         if (msgs[i].type==="message"){
             divChat.innerHTML+=`
             <div class="mensagem">
-                (${msgs[i].time}) <strong>${msgs[i].from}</strong> para <strong>${msgs[i].to}</strong>: ${msgs[i].text}
+                <span>(${converteHora(msgs[i].time)})</span> <strong>${msgs[i].from}</strong> para <strong>${msgs[i].to}</strong>: ${msgs[i].text}
             </div>`;
         }
         if (msgParaMim()){
             divChat.innerHTML+=`
             <div class="mensagem reservada">
-                (${msgs[i].time}) <strong>${msgs[i].from}</strong> reservadamente para <strong>${msgs[i].to}</strong>: ${msgs[i].text}
+                <span>(${converteHora(msgs[i].time)})</span> <strong>${msgs[i].from}</strong> reservadamente para <strong>${msgs[i].to}</strong>: ${msgs[i].text}
             </div>`;
         }
         const ultimaMsg=document.querySelector(".conteiner-chat").lastElementChild;
@@ -77,10 +88,14 @@ function msgParaMim(){
 
 function cadastraNome(){
     nomeUser=document.querySelector(".nome-user").value
+    document.querySelector(".login").classList.add("escondido")
+    document.querySelector(".carregando").classList.remove("escondido")
     inicio()
 }
 
 function tratarErroNome(){
+    document.querySelector(".carregando").classList.add("escondido")
+    document.querySelector(".login").classList.remove("escondido")
     const caixaNomeUser=document.querySelector(".nome-user")
     caixaNomeUser.classList.add("existente")
     caixaNomeUser.setAttribute("placeholder", "Nome de usuário indisponível");
