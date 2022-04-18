8d41975796ae51ca415f1e7208a2a759b3f5ce0a
Vitor Carvalho Matos <vitorcarvalhomatos@gmail.com>
2022-04-18 12:21:56 -0300

    Versão Completa

--- script.js
+++ script.js
@@ -1,33 +1,47 @@
+//Variaveis globais:
 let nomeUser;
 let alvoMsg= "Todos";
 let tipoTraduzido= "Público";
 let tipoMsg="message";
 let msgs=[];
 let nomeContatos=[];
+let ultimaMsg=""
 
-
+//Mapeia o uso do Enter na janela inicial
 document.querySelector(".janela-inicial").addEventListener("keydown",function(e){
     if (e.key==="Enter"){
-        cadastraNome();
+        inicio();
     }
 });
 
+//Mapeia o uso do Enter na janela principal
 document.querySelector(".janela-principal").addEventListener("keydown",function(e){
     if (e.key==="Enter"){
         enviaMsg();
     }
 });
 
+//Responsavel pelas ações da tela de inicio do aplicativo
 function inicio(){
+    nomeUser=document.querySelector(".nome-user").value;
+    document.querySelector(".login").classList.add("escondido");
+    document.querySelector(".carregando").classList.remove("escondido");
+    cadastraNome();
+}
+
+//Cadastra usuário na API
+function cadastraNome(){
     const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", {name: nomeUser});
     promise.then(iniciaChat);
     promise.catch(tratarErroNome);
 }
 
+//Renova o status online na API
 function renovaStatus(){
     axios.post("https://mock-api.driven.com.br/api/v6/uol/status", {name: nomeUser});
 }
 
+//Inicia as ações periodicas para o funcionamento do aplicativo 
 function iniciaChat(){
     carregaContatos();
     setInterval(renovaStatus,5000);
@@ -35,16 +49,19 @@ function iniciaChat(){
     setInterval(carregaContatos,10000);
 }
 
+//Pede os contatos disponiveis para a API
 function carregaContatos(){
     const promise=axios.get("https://mock-api.driven.com.br/api/v6/uol/participants");
     promise.then(carregaNomes);
 }
 
+//Carrega os nomes que a API retornou
 function carregaNomes(response){
     nomeContatos= response.data;
     renderizarNomes();
 }
 
+//Mostra os nomes que foram carregados
 function renderizarNomes(){
     const divContatos=document.querySelector(".lista-contatos");
     let opcao="";
@@ -76,6 +93,7 @@ function renderizarNomes(){
     } 
 }
 
+//Atualiza o tipo de mensagem que sera enviado para API
 function atualizaTipoMsg(){
     if(tipoTraduzido==="Público"){
         tipoMsg="message";
@@ -85,6 +103,7 @@ function atualizaTipoMsg(){
     }
 }
 
+//Responsavel por definir para quem a mensagem vai ser enviada
 function escolheAlvoMsg(elemento){
     if(document.querySelector(".lista-contatos .marcado")!== null){
         document.querySelector(".lista-contatos .marcado").classList.remove("marcado");
@@ -94,6 +113,7 @@ function escolheAlvoMsg(elemento){
     document.querySelector(".barra-inferior h2").innerHTML=`Enviando para ${alvoMsg} (${tipoTraduzido})`;
 }
 
+//Responsavel por definir o tipo de mensagem a ser enviada
 function escolheVisibilidade(elemento){
     if(document.querySelector(".visibilidade .marcado")!== null){
         document.querySelector(".visibilidade .marcado").classList.remove("marcado");
@@ -104,11 +124,13 @@ function escolheVisibilidade(elemento){
     atualizaTipoMsg();
 }
 
+//Pede as mensagens para a API
 function carregaChat(){
     const promise=axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
     promise.then(carregaMsgs);
 }
 
+//Carrega as mensagens que a API retornou
 function carregaMsgs(response){
     document.querySelector(".janela-inicial").classList.add("escondido");
     document.querySelector(".janela-principal").classList.remove("escondido");
@@ -116,10 +138,11 @@ function carregaMsgs(response){
     renderizarMsgs();
 }
 
+//Converte a hora da API
 function converteHora(hora){
-    let horaAtual=Number(hora.slice(0,2))+9;
-    if (horaAtual>=24){
-        horaAtual-=24;
+    let horaAtual=Number(hora.slice(0,2))-3;
+    if (horaAtual<0){
+        horaAtual+=24;
     }
     if(String(horaAtual).length<2){
         horaAtual="0"+horaAtual;
@@ -127,6 +150,7 @@ function converteHora(hora){
     return horaAtual+hora.slice(2,hora.length);
 }
 
+//Mostra as mensagens que foram carregadas
 function renderizarMsgs(){
     const divChat=document.querySelector(".conteiner-chat");
     divChat.innerHTML="";
@@ -150,11 +174,15 @@ function renderizarMsgs(){
                 <span>(${converteHora(msgs[i].time)})</span> <strong>${msgs[i].from}</strong> reservadamente para <strong>${msgs[i].to}</strong>: ${msgs[i].text}
             </div>`;
         }
-        const ultimaMsg=document.querySelector(".conteiner-chat").lastElementChild;
-        ultimaMsg.scrollIntoView();
+
+        if(ultimaMsg!==document.querySelector(".conteiner-chat").lastElementChild){
+            ultimaMsg=document.querySelector(".conteiner-chat").lastElementChild;
+            ultimaMsg.scrollIntoView();
+        }
     }
 }
 
+//Verifica se o usuário deve ou não ter acesso a uma mensagem privada
 function msgParaMim(){
     if (msgs[i].type==="private_message" && (msgs[i].from===nomeUser|| msgs[i].to===nomeUser || msgs[i].to==="Todos")){
         return true;
@@ -162,13 +190,7 @@ function msgParaMim(){
     return false;
 }
 
-function cadastraNome(){
-    nomeUser=document.querySelector(".nome-user").value;
-    document.querySelector(".login").classList.add("escondido");
-    document.querySelector(".carregando").classList.remove("escondido");
-    inicio();
-}
-
+//Responsavel pelas ações caso o nome de usuário escolhido esteja indisponivel
 function tratarErroNome(){
     document.querySelector(".carregando").classList.add("escondido");
     document.querySelector(".login").classList.remove("escondido");
@@ -178,10 +200,12 @@ function tratarErroNome(){
     caixaNomeUser.value="";
 }
 
+//Faz a janela de usuário se mostrar
 function janelaUsers(){
     document.querySelector(".janela-users").classList.toggle("mostrar");
 }
 
+//Envia mensagens para a API
 function enviaMsg(){
     const textoMsg= document.querySelector(".caixa-msg").value;
     const promise=axios.post("https://mock-api.driven.com.br/api/v6/uol/messages",{
